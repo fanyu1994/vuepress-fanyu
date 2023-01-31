6c2448d3a239a51dc4b056ff7362e1841559bd47
fanyu <1025153697@qq.com>
2023-01-31 17:29:52 +0800

    feat:init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  title: 'TypeScript4 文档',
+  description: 'TypeScript4 最新官方文档翻译',
+  themeConfig: {
+    logo: '/img/fanyulogo.png',
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: '烤鱼 博客',
+        items: [
+          { text: 'Github', link: 'https://github.com/fanyu1994' },
+          {
+            text: '掘金',
+            link: 'https://juejin.cn/user/2576910988874808',
+          },
+        ],
+      },
+    ],
+    sidebar:[
+      {
+        title: '学前班',
+        path: '/',
+        collapsable: false, // 不折叠
+        children: [
+            { title: "学前必读", path: "/" }
+        ]
+      },
+      {
+        title: "基础学习",
+        path: '/basebook/ConditionalTypes',
+        collapsable: false, // 不折叠
+        children: [
+          { title: "条件类型", path: "/basebook/ConditionalTypes" },
+          { title: "泛型", path: "/basebook/Generics" }
+        ],
+      }
+    ]
+  },
+}
